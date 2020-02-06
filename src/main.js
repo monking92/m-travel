0f061f5d1bf140d3fbe990658075b0d323483e41
monking92 <qzsunlei@163.com>
2020-02-06 20:18:17 +0800

    [add] Home header

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/styles/reset.css'
+import '@/assets/styles/border.css'
+import '@/assets/styles/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <i class="iconfont back-icon">&#xe658;</i>
+    </div>
+    <div class="header-center">
+      <i class="iconfont">&#xe63c;</i>
+      <input type="text" placeholder="输入城市/景点/游玩主题">
+      <!-- 输入城市/景点/游玩主题 -->
+    </div>
+    <div class="header-right">
+      <span>城市</span>
+      <i class="iconfont">&#xe65c;</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/variables.styl'
+.header {
+  display: flex;
+  line-height: .86rem;
+  background: $bgColor;
+  color: #fff;
+  .header-left {
+    width: .64rem;
+    text-align: center;
+    .back-icon {
+      font-size: .4rem
+    }
+  }
+  .header-center {
+    flex: 1;
+    height: .64rem;
+    line-height: .64rem;
+    margin-top: .12rem;
+    // margin-left: .2rem;
+    border-radius: .1rem;
+    background: #fff;
+    color: #ccc;
+  }
+  .header-right {
+    width: 1.24rem;
+    text-align: center;
+  }
+}
+</style>
